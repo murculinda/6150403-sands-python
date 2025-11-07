e4a9d8f4faa5983e7fd7c160bc4b9adc2d5a2759
Marcel Nowak <murculinda@gmail.com>
2025-11-07 19:12:08 +0100

    Fixing sine test function

--- test_my_function.py
+++ test_my_function.py
@@ -1,8 +1,8 @@
 import numpy as np
-from sine_function import create_sine_wave as generate_sine_wave
+from sine_function import create_sine_wave
 
 
-def test_generate_sine_wave():
+def test_create_sine_wave():
     t, y = create_sine_wave(1, 2, 1)
     assert len(t) == 1000
     assert y[0] == 0
@@ -10,7 +10,7 @@ def test_generate_sine_wave():
     t, y = create_sine_wave(5, 3, 1)
     assert np.isclose(max(y), 3, atol=1e-6)
 
-    t, y = creat_sine_wave(1, 2, -1)
+    t, y = create_sine_wave(1, 2, -1)
     assert len(t) == 0 and len(y) == 0
 
     t, y = create_sine_wave(5, 0, 1)
